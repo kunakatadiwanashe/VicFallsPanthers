9fc8dc7fa834e090671f9e828189fcadfaa733c7
kunakatadiwanashe <kunakatadiwanashe99@gmail.com>
2024-11-08 17:02:43 +0200

    icon

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,6 @@ import React from "react";
 import Footer from "@/components/footer";
 import PlayerStats2 from '../components/playerStats2';
 import BoysTeam from "@/components/boysTeam";
-import FunClub from "@/components/joinFanClub";
 import NextMatch from "@/components/nextMatch";
 import Banner from "@/components/Banner";
 
