d452681c55dc13a60362cd78e99b7c0e55a15cc2
kunakatadiwanashe <kunakatadiwanashe99@gmail.com>
2024-11-11 15:50:01 +0200

    rf

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,5 +1,4 @@
 import { Disclosure, DisclosureButton, DisclosurePanel, Menu, MenuButton, MenuItem, MenuItems } from '@headlessui/react'
-// import { Bars3Icon, BellIcon, XMarkIcon } from '@heroicons/react/24/outline'
 import Image from 'next/image'
 import { FaBars } from "react-icons/fa";
 import { CiBookmarkCheck } from "react-icons/ci";
