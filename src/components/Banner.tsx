78f1678296334a22f07aa861482258204b2664ae
kunakatadiwanashe <kunakatadiwanashe99@gmail.com>
2024-11-11 17:35:39 +0200

    rmv 5:30

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -1,40 +1,35 @@
 import React from "react";
 
-
-
 import BannerSlide from "./BannerSlide";
 import Navbar from "./Navbar";
-
 
 // style={{ backgroundImage: `url(${bg.src})` }}
 
 const Banner = () => {
-    return (
-        <div className="min-h-screen overflow-x-hidden bg-cover bg-center" >
+  return (
+    <div className="min-h-screen overflow-x-hidden bg-cover bg-center">
+      <Navbar />
 
-
-            <Navbar />
-
-            <main className="flex flex-col items-center md:flex-row md:items-center md:justify-between p-4 md:p-12 lg:p-0">
-                <div className="w-full md:w-[50%] p-4 md:p-16 pt-10 md:pt-40 lg:pt-4 text-[#118985] text-center md:text-left">
-                    <h1 className="text-4xl md:text-6xl font-bold mb-4 drop-shadow-2xl">
-                        Victoria Falls Panthers Basketball
-                    </h1>
-                    <p className="mb-6 text-lg md:text-xl text-black">
-                        Premier Amateur Basketball Club in Victoria Falls, Zimbabwe
-                        affiliated to the Matabeleland North Basketball League.
-                    </p>
-   5:30                 <button className="bg-[#118985] text-white px-4 py-2 md:px-6 md:py-3 rounded">
-                        Get Started →
-                    </button>
-                </div>
-
-                <div className="relative mt-6 lg:mt-0 md:mt-0 w-full md:w-[50%] pr-0 md:pr-28 lg:h[100vh]">
-                    <BannerSlide />
-                </div>
-            </main>
+      <main className="flex flex-col items-center md:flex-row md:items-center md:justify-between p-4 md:p-12 lg:p-0">
+        <div className="w-full md:w-[50%] p-4 md:p-16 pt-10 md:pt-40 lg:pt-4 text-[#118985] text-center md:text-left">
+          <h1 className="text-4xl md:text-6xl font-bold mb-4 drop-shadow-2xl">
+            Victoria Falls Panthers Basketball
+          </h1>
+          <p className="mb-6 text-lg md:text-xl text-black">
+            Premier Amateur Basketball Club in Victoria Falls, Zimbabwe
+            affiliated to the Matabeleland North Basketball League.
+          </p>
+          <button className="bg-[#118985] text-white px-4 py-2 md:px-6 md:py-3 rounded">
+            Get Started →
+          </button>
         </div>
-    );
+
+        <div className="relative mt-6 lg:mt-0 md:mt-0 w-full md:w-[50%] pr-0 md:pr-28 lg:h[100vh]">
+          <BannerSlide />
+        </div>
+      </main>
+    </div>
+  );
 };
 
 export default Banner;
