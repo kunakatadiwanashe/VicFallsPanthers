9f967fbcd685cb167ca8cfef87ca7931125e40b2
kunakatadiwanashe <kunakatadiwanashe99@gmail.com>
2024-11-26 14:10:06 +0200

    useefect

--- src/components/SocialMedia.tsx
+++ src/components/SocialMedia.tsx
@@ -1,7 +1,3 @@
-"use client";
-import { useEffect, useState } from "react";
-import Image from "next/image";
-
 
 const SocialMedia = () => {
 
@@ -11,16 +7,11 @@ const SocialMedia = () => {
       <h2 className="text-3xl pl-4 font-bold">Social Media</h2>
 
       <div>
-   
       <iframe src="https://widget.taggbox.com/2137726" className="w-[100vw] h-[120vh] overflow-hidden"></iframe>
 
       {/* <div  styles="width:100%;height:100%" data-widget-id="2137726" data-tags="false"  ></div>
       <script src="https://widget.taggbox.com/embed-lite.min.js" type="text/javascript"></script> */}
-   
-   
-   
-   
-         </div>
+   </div>
 
 
     </div>
